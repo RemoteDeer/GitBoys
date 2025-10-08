2afa32b55e9b6c650967da204a4d5e278979ae11
uudx <amambaynurzhan@gmail.com>
2025-10-09 00:37:21 +0500

    incompleted map

--- main/MicroWork/map/map.js
+++ main/MicroWork/map/map.js
@@ -1,3 +1,7 @@
+import data from '/GitBoys/main/MicroWork/data/data.json' with {type: 'json'};
+
+const obj = JSON.parse(data);
+
 let latitude = 51.087063;
 let longtitude = 71.427878;
 let zoom = 13;
@@ -9,9 +13,16 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(mymap);
 
-function onMapClick(e) {
+var marker = L.marker([latitude, longtitude]).addTo(mymap);
+marker.bindPopup("first popup", {autoClose: true, closeOnClick: true}).openPopup();
+
+function onMapClick(e, name, location, discription) {
     var marker = L.marker(e.latlng).addTo(mymap);
-    marker.bindPopup("You clicked the map at LatLng" + e.latlng).openPopup();
+    marker.bindPopup("You clicked the map at LatLng" + e.latlng, {autoClose: true, closeOnClick: true}).openPopup();
+}
+
+for(let i = 0; i < 10; i++){
+    alert(obj[i].name + " " + obj.position + " " + obj.description);
 }
 
 mymap.on('click', onMapClick);
