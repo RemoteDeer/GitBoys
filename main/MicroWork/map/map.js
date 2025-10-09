22c112ccbe519479ec0a396fcd1e1f1c3f4dc382
uudx <amambaynurzhan@gmail.com>
2025-10-09 21:27:06 +0500

    Added the ability to create popups using simple json

--- main/MicroWork/map/map.js
+++ main/MicroWork/map/map.js
@@ -1,28 +1,36 @@
-import data from '/GitBoys/main/MicroWork/data/data.json' with {type: 'json'};
-
-const obj = JSON.parse(data);
-
 let latitude = 51.087063;
 let longtitude = 71.427878;
 let zoom = 13;
 
-var mymap = L.map('mapid').setView([latitude, longtitude], zoom);
-
+const mymap = L.map('mapid').setView([latitude, longtitude], zoom);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(mymap);
 
-var marker = L.marker([latitude, longtitude]).addTo(mymap);
-marker.bindPopup("first popup", {autoClose: true, closeOnClick: true}).openPopup();
 
-function onMapClick(e, name, location, discription) {
-    var marker = L.marker(e.latlng).addTo(mymap);
-    marker.bindPopup("You clicked the map at LatLng" + e.latlng, {autoClose: true, closeOnClick: true}).openPopup();
+function addMarkersToMap(locations) {
+    locations.forEach(location => {
+        const marker = L.marker([location.position[0], location.position[1]])
+            .addTo(mymap)
+            .bindPopup(`<b>${location.name}</b><br>${location.description}`).openPopup();
+    });
 }
 
-for(let i = 0; i < 10; i++){
-    alert(obj[i].name + " " + obj.position + " " + obj.description);
+function loadData() {
+    fetch('./data/data.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json(); 
+        })
+        .then(data => {
+            addMarkersToMap(data); 
+        })
+        .catch(error => {
+            console.error("Ошибка при загрузке или обработке JSON:", error);
+        });
 }
 
-mymap.on('click', onMapClick);
+loadData();
